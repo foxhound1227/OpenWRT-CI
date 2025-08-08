e167ab103cc071dc6671a3541e32500fc56ee505
foxvip <bruce1227@gmail.com>
2025-08-08 09:13:21 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -77,52 +77,12 @@ if [ -f "$RUST_FILE" ]; then
 	cd $PKG_PATH && echo "rust has been fixed!"
 fi
 
-
-#移除Shadowsocks组件
-PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall/Makefile")
-if [ -f "$PW_FILE" ]; then
-	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/x86_64/d' $PW_FILE
-	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW_FILE
-	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $PW_FILE
-
-	cd $PKG_PATH && echo "passwall has been fixed!"
-fi
-
-SP_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-ssr-plus/Makefile")
-if [ -f "$SP_FILE" ]; then
-	sed -i '/default PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/libev/d' $SP_FILE
-	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/x86_64/d' $SP_FILE
-	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $SP_FILE
-
-	cd $PKG_PATH && echo "ssr-plus has been fixed!"
-fi
-
-#修复Coremark编译失败
-CM_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/coremark/Makefile")
-if [ -f "$CM_FILE" ]; then
-	sed -i 's/mkdir/mkdir -p/g' $CM_FILE
-
-	cd $PKG_PATH && echo "coremark has been fixed!"
-fi
-
 #修复DiskMan编译失败
-#DM_FILE="./luci-app-diskman/applications/luci-app-diskman/Makefile"
-#if [ -f "$DM_FILE" ]; then
-#	echo " "
-#
-#	sed -i 's/fs-ntfs/fs-ntfs3/g' $DM_FILE
-#
-#	cd $PKG_PATH && echo "diskman has been fixed!"
-#fi
-
-#修复rpcsvc-proto编译失败
-RP_PATH="../feeds/packages/libs/rpcsvc-proto"
-if [ -d "$RP_PATH" ]; then
+DM_FILE="./luci-app-diskman/applications/luci-app-diskman/Makefile"
+if [ -f "$DM_FILE" ]; then
 	echo " "
 
-	cd $RP_PATH && mkdir -p patches && cd ./patches
+	sed -i 's/fs-ntfs/fs-ntfs3/g' $DM_FILE
 
-	curl -sL -o "0001-po-update-for-gettext-0.22.patch" https://raw.githubusercontent.com/neheb/packages/refs/heads/mangix/libs/rpcsvc-proto/patches/0001-po-update-for-gettext-0.22.patch
-
-	cd $PKG_PATH && echo "rpcsvc-proto has been fixed!"
+	cd $PKG_PATH && echo "diskman has been fixed!"
 fi
